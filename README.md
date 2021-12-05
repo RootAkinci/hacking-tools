7733b19557fb5784be6fe09fb4de1d938e018597
Root Akıncı <89400282+RootAkinci@users.noreply.github.com>
2021-12-05 15:34:33 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hacking Tools
+
+Sosyal Medya Hesaplarım.
+--------------------------------------------------------------------------
+# İnstagram: https://instagram.com/rootakinci
+# Telegram : https://t.me/rootakinci
+--------------------------------------------------------------------------
---
+++ hacking-tools.py
@@ -0,0 +1,163 @@
+#Coded by Root Akıncı
+#instagram.com/rootakinci
+#t.me/rootakinci
+
+import os
+
+os.system("clear")
+
+
+print("──────█─────────▐▌──────────▐▌────────")
+print("         Code: Root Akıncı            ")
+print("         İnstagram: rootakinci        ")
+print("         Telegram: rootakinci         ")
+print("──────█─────────▐▌──────────▐▌────────")
+
+print("")
+print("")
+print("1.Sistem Güncelle")
+print("2.Ngrok")
+print("3.SQL-scanner")
+print("4.Sqlmap")
+print("5.Admin Panel Finder")
+print("6.Metasploit (Kali Linux)")
+print("7.IPGeoLocation")
+print("8.Tc-Kimlik")
+print("9.Osi.İg")
+print("10.FindUser")
+print("11.Nmap (Kali Linux)")
+print("12.A-Rat")
+print("13.CamPhish")
+print("14.OSIF")
+print("15.PhoneInfoga")
+print("16.Hasher")
+print("17.Hash-identifier")
+print("18.Cupp")
+print("19.Wpscan (Kali Linux)")
+print("20.Cadaver (Kali Linux)")
+print("21.Hydra (Kali Linux)")
+print("22.Nikto (Kali Linux)")
+print("23.Hashcat (Kali Linux)")
+print("24.Hulk")
+print("25.ReconCobra")
+print("26.Killshot")
+print("27.Commix")
+print("28.WAES")
+print("29.ShellPhish")
+print("30.Beef")
+print("31.Sublist3r")
+print("32.XSStrike")
+print("")
+print("")
+
+islem = input("İşlem Numarası Giriniz: ")
+
+if islem =="1":
+    os.system("apt-get update && apt-get -y full-upgrade")
+
+if islem =="2":
+    os.system("apt-get install ngrok")
+
+if islem =="3":
+    os.system("git clone https://github.com/Bitwise-01/SQL-scanner")
+
+if islem =="4":
+    os.system("sqlmap")
+
+if islem =="5":
+    os.system("git clone https://github.com/s0md3v/Breacher")
+
+if islem =="6":
+    os.system("msfconsole")
+
+if islem =="7":
+    os.system("git clone https://github.com/maldevel/IPGeoLocation")
+
+if islem =="8":
+    os.system("git clone https://github.com/ibrahimirdem/tckimlik")
+
+if islem =="9":
+    os.system("git clone https://github.com/th3unkn0n/osi.ig")
+
+if islem =="10":
+    os.system("git clone https://github.com/xHak9x/finduser")
+
+if islem =="11":
+    os.system("nmap")
+
+if islem =="12":
+    os.system("git clone https://github.com/RexTheGod/A-Rat")
+
+if islem =="13":
+    os.system("git clone https://github.com/techchipnet/CamPhish")
+
+if islem =="14":
+    os.system("git clone https://github.com/CiKu370/OSIF")
+
+if islem =="15":
+    os.system("git clone https://github.com/sundowndev/phoneinfoga")
+
+if islem =="16":
+    os.system("git clone https://github.com/ciku370/hasher")
+
+if islem =="17":
+    os.system("apt-get install hash-identifier")
+
+if islem =="18":
+    os.system("git clone https://github.com/Mebus/cupp")
+
+if islem =="19":
+    os.system("wpscan --help")
+
+if islem =="20":
+    os.system("cadaver")
+
+if islem =="21":
+    os.system("hydra")
+
+if islem =="22":
+    os.system("nikto")
+
+if islem =="23":
+    os.system("hashcat")
+
+if islem =="24":
+    os.system("git clone https://github.com/grafov/hulk")
+
+if islem =="25":
+    os.system("git clone https://github.com/Agent00049/ReconCobra")
+
+if islem =="26":
+    os.system("git clone https://github.com/bahaabdelwahed/killshot")
+
+if islem =="27":
+    os.system("git clone https://github.com/commixproject/commix")
+
+if islem =="28":
+    os.system("git clone https://github.com/Shiva108/WAES")
+
+if islem =="29":
+    os.system("git clone https://github.com/suljot/shellphish")
+
+if islem =="30":
+    os.system("git clone https://github.com/beefproject/beef")
+
+if islem =="31":
+    os.system("git clone https://github.com/aboul3la/Sublist3r")
+
+if islem =="32":
+    os.system("git clone https://github.com/s0md3v/XSStrike")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
